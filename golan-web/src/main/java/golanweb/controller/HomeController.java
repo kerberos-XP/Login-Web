20fba0ef17ce6ff7aa102f2c014023d72ea0e6b9
Omar Paché <omar.pache@gmail.com>
2015-11-29 18:38:30 -0300

    Se agrega log for gay

--- golan-web/src/main/java/golanweb/controller/HomeController.java
+++ golan-web/src/main/java/golanweb/controller/HomeController.java
@@ -2,25 +2,30 @@ package golanweb.controller;
 
 
 import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Controlador principal responsable de servir las peticiones
+ * Controlador principal responsable de enrutar las peticiones
  * 
  * @author Omar Paché
  */
 @Controller
 public class HomeController {
-
+    
+    private final Logger log = Logger.getLogger(HomeController.class);
+    
     @RequestMapping(value = {"/", "home"})
     public String inicio(HttpServletResponse response) {
+        log.info("retornando pagina home");
         return "home";
     }
 
     @RequestMapping("/recuperacion")
     public String recuperacion() {
+        log.info("retornando pagina recuperacion");
         return "recuperacion";
     }
 }
